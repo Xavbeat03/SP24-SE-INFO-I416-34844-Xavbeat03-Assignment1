6baade84df6e66cbeaec85c8c72fbd41b0aa9274
Xavier Beatrice <Xavbeat03@gmail.com>
2024-02-10 14:28:35 -0500

    Adding a Client Object for the server to use and starting to setup the server socket programming.

---
+++ src/main/java/server/Client.java
@@ -0,0 +1,17 @@
+package server;
+
+/**
+ * A client object that the server can create upon connecting to a new client to store information about it.
+ */
+public class Client {
+
+	static private int latestId = 0;
+	int id = latestId;
+	Client() {
+		id = latestId++;
+	}
+
+	int getId(){
+		return id;
+	}
+}
