360f6d409f0ebf90c495afa14d603cd3313ea4f6
Xavier Beatrice <Xavbeat03@gmail.com>
2024-03-06 03:46:58 -0500

    Gotta make the RequestFulfiller wait properly, perhaps wait and notify?

--- src/main/java/server/requests/RequestFulfiller.java
+++ src/main/java/server/requests/RequestFulfiller.java
@@ -4,11 +4,13 @@ import server.client.Client;
 import server.file.FileHandler;
 
 public class RequestFulfiller extends Thread{
+    private boolean fulfillingRequest = false;
 
     @Override
     public void run() {
         // Start looping
         while(true){
+            fulfillingRequest = false;
             Request r = null;
             // get the next request
             try {
@@ -28,6 +30,7 @@ public class RequestFulfiller extends Thread{
         if (r.getRequestType() == RequestType.SET) {
             // set the value of the key in the database
             try {
+                fulfillingRequest = true;
                 FileHandler.storeValue(r.getKey(), ((SetRequest) r).getValue());
                 Client.getClientById(r.getClientId()).getClientHandler().outputMessage("STORED\r\n");
             } catch (Exception e) {
@@ -38,6 +41,7 @@ public class RequestFulfiller extends Thread{
         } else if (r.getRequestType() == RequestType.GET) {
             // get the value of the key from the database
             try {
+                fulfillingRequest = true;
                 String[] key = FileHandler.getValue(r.getKey());
                 Client.getClientById(r.getClientId()).getClientHandler().outputMessage("VALUE %s %d \r\n".formatted(r.getKey(), r.getKey().length()));
                 Client.getClientById(r.getClientId()).getClientHandler().outputMessage("%s \r\n".formatted(key[1]));
